c0431c0fe88f085a2bc5277ffc2b9ec1b3d58e2f
Jorge Fierro <jfpal93@hotmail.com>
2016-01-28 08:42:19 -0500

    Control de reservacion

---
+++ Bases del Proyecto/Control_Reservacion.sql
@@ -0,0 +1,41 @@
+use ProyectoBD;
+
+
+delimiter |
+create procedure insertar_Reservacion(in fech date, in des varchar(45),in cant int, in prec double, in idEv int,in idC int)
+begin 
+	set foreign_key_checks=0;
+	insert into Reservacion (fecha,descripcion,cantidad,precio,Eventos_idEventos,Cliente_idCliente)values( fech ,  des , cant,  prec ,  idEv,idC );
+	set foreign_key_checks=1;
+end
+|
+delimiter ;
+
+
+
+delimiter |
+create view Mostrar_Reservacion  as
+	select * from Reservacion ;
+
+|
+delimiter ;
+
+delimiter |
+create procedure Editar_Reservacion (in idR int,in fech date, in des varchar(45),in cant int, in prec double, in idEv int,in idC int)
+begin 
+	set foreign_key_checks=0;
+	update Reservacion  set fecha=fech,descripcion=des,cantidad=cant,precio=prec,Eventos_idEventos=idEv,Cliente_idCliente=idC where idReservacion=idR;
+	set foreign_key_checks=0;
+end
+|
+delimiter ;
+
+delimiter |
+create procedure Borrar_Reservacion (in idR int)
+begin 
+	
+	delete from Reservacion  where idReservacion=idR;
+	
+end
+|
+delimiter ;
